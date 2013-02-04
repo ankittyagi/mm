027ead09f9c938cf159eb6636418670545dec5f3
ankit.tyagi <ankit.tyagi@ballisticlearning.com>
2013-02-04 12:36:56 +0530

    index file templates seprated

--- plugins/profile/main.js
+++ plugins/profile/main.js
@@ -9,7 +9,12 @@ define(templates,function (profileTpl,profileTp2) {
             name: "profile",
             type: "general",
 			menuURL: "#profile/viewprofile",
-		    lang: {
+			//subMenus: [
+              //  {name: "viewprofile", menuURL: "#profile/viewprofile", icon: "plugins/profile/profile.png"},
+             // {name: "editprofile", menuURL: "#profile/editprofile", icon: "plugins/profile/profile.png"},
+              
+         //   ],
+            lang: {
                 component: "core"
             }
         },
@@ -25,6 +30,7 @@ define(templates,function (profileTpl,profileTp2) {
 		],
 		
 		showProfile: function() {
+
 			 var siteid=MM.config.current_site.id;
 			 var site = MM.db.get("sites", siteid);
 			 var userid= site.get('userid');
@@ -44,56 +50,79 @@ define(templates,function (profileTpl,profileTp2) {
                 var html = MM.tpl.render(MM.plugins.profile.templates.profile.html, tpl);
                 MM.panels.show('center', html);  
 			
+			//	if (MM.deviceType == "tablet") {
+               //     MM.plugins.profile.editProfile(userid);
+             //   }
+			
             }); 
         
 		   },
 		editProfile: function() {
-			var siteid=MM.config.current_site.id;
+			 var siteid=MM.config.current_site.id;
 			 var site = MM.db.get("sites", siteid);
 			 var userid= site.get('userid');
-            MM.panels.showLoading('center');
+			 
+           MM.panels.showLoading('center');
             
-            if (MM.deviceType == "tablet") {
-                MM.panels.showLoading('right');
-            }
+         //   if (MM.deviceType == "tablet") {
+           //     MM.panels.showLoading('right');
+          //  }
     
             var data = {
                 "userids[0]" : userid 
             };
             
-            MM.moodleWSCall('moodle_user_get_users_by_id', data, function(users) {   
+            MM.moodleWSCall('moodle_user_get_users_by_id', data, function(users) {    // local_usersget_getuser_by_id 
 			    var tpl = {users: users, deviceType: MM.deviceType,"user": users.shift()};
                 var html = MM.tpl.render(MM.plugins.profile.templates.editprofile.html, tpl);
-                MM.panels.show('center', html);  
+                MM.panels.show('right', html);  
 			
             });
 		
            },
 		   
 		 updateProfile: function() {
+			
+		// alert("updateprofile");
 		  
 			 var siteid=MM.config.current_site.id;
 			 var site = MM.db.get("sites", siteid);
 			 var userid= site.get('userid');
 			MM.panels.showLoading('center');
-			
-			// Just updating constant email right now for all users
-			
 			var data =
 			{
-			"users[0][id]" : userid
-			"users[0][email]" : 'anky@localhost.com',
+			"users[0][id]" : userid,
+			//"users[0][email]" : 'anky@localhost.com',
+			/*
+			"users[0][username]": string
+			"users[0][password]":string
+			"users[0][firstname]": string
+			"users[0][lastname]": string
+			"users[0][email]": string
+			"users[0][auth]": string
+			"users[0][idnumber]": string
+			"users[0][lang]": string
+			"users[0][theme]": string
+			"users[0][timezone]": string
+			"users[0][mailformat]": int
+			"users[0][description]": string
+			"users[0][city]": string
+			"users[0][country]": string
+			"users[0][customfields][0][type]": string
+			"users[0][customfields][0][value]": string
+			"users[0][preferences][0][type]": string
+			"users[0][preferences][0][value]": string				
+			*/
 			};
 			// web service not working still finding the BUG
-			/*
-			MM.moodleWSCall('core_user_update_users', data, function(){
-			   MM.popMessage(MM.lang.s("profileupdated"));  	  
+			MM.moodleWSCall('moodle_user_update_users', data, function(){
+			  alert("ho gaya");
+			  // MM.popMessage(MM.lang.s("profileupdated"));  	  
 			});
-			*/
-			
-			  MM.popMessage(MM.lang.s("Not working still finding the BUG"));   
-				MM.Router.navigate("profile/viewprofile");
-				setInterval(location.reload(),3000);
+			//alert("Not working still finding the BUG Click OK to Redirect ");
+			//navigator.app.exitApp();
+			//	MM.Router.navigate("profile/viewprofile");
+				//setInterval(location.reload(),3000);
 				
 				
 			},
